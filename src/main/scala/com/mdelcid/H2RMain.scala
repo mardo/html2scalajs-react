88da423011802fac45d9005fbd35d96a5aad7cb0
Mardo Del Cid <mardodcp@gmail.com>
2016-08-09 21:43:26 -0600

    Better HTML parsing

--- src/main/scala/com/mdelcid/H2RMain.scala
+++ src/main/scala/com/mdelcid/H2RMain.scala
@@ -3,36 +3,49 @@ package com.mdelcid
 import com.mdelcid.models.H2RNode
 
 import scala.scalajs.js.annotation.JSExport
-
+import scala.scalajs.js
 import org.scalajs.{dom, jquery}
+import org.scalajs.dom.window.console
 
 @JSExport
 object H2RMain {
-  val $ = jquery.jQuery
+	val $ = jquery.jQuery
 
-  @JSExport
-  def main(): Unit = {
+	@JSExport
+	def main(): Unit = {
 
-    $("#html-textarea").click((e: dom.Event) => {
-      $("#html-textarea").select()
-    })
-    $("#convert-button").click((e: dom.Event) => {
-      val htmlStr = $("#html-textarea").`val`().toString
+		$("#html-textarea").click((e: dom.Event) => {
+			$("#html-textarea").select()
+		})
+		$("#convert-button").click((e: dom.Event) => {
+			val htmlStr = $("#html-textarea").`val`().toString
 
-      try {
-        val html = $.parseXML(htmlStr).asInstanceOf[dom.Node]
+			try {
 
-        val scalaJSReactCode = H2RNode(html, -1).childrenStr
-        $("#html-textarea").`val`(scalaJSReactCode)
-        $("#html-textarea").select()
-      } catch {
-        case e: Throwable =>
-          e.printStackTrace()
-          dom.window.alert("There was an error parsing the HTML. Please check your input is valid.")
-      }
-    })
+				val htmlArray = $.parseHTML(htmlStr).asInstanceOf[js.Array[dom.Node]]
 
-  }
+				js.Dynamic.global.vvv = htmlArray
+
+				val rootNode = new js.Object {
+					val nodeName = "#root"
+					val childNodes = htmlArray
+					val textContent = ""
+				}.asInstanceOf[dom.Node]
+
+				console.log(rootNode)
+
+				val scalaJSReactCode = H2RNode(rootNode, 0).childrenStr
+
+				$("#html-textarea").`val`(scalaJSReactCode)
+				$("#html-textarea").select()
+			} catch {
+				case e: Throwable =>
+					e.printStackTrace()
+					dom.window.alert("There was an error parsing the HTML. Please check your input is valid.")
+			}
+		})
+
+	}
 
 
 }
--- src/main/scala/com/mdelcid/models/H2RNode.scala
+++ src/main/scala/com/mdelcid/models/H2RNode.scala
@@ -1,8 +1,10 @@
 package com.mdelcid.models
 
 import com.mdelcid.AttributeNamesMapping
+import org.scalajs
 import org.scalajs.dom
 import org.scalajs.dom.Node
+import org.scalajs.dom.window.console
 
 import scala.scalajs.js
 import scala.scalajs.js.UndefOr
@@ -11,11 +13,17 @@ import scala.scalajs.js.UndefOr
   * Created by mardo on 8/6/16.
   */
 
+
 case class H2RNode(xml: dom.Node, depth: Integer) {
 
-	val children: Seq[H2RNode] = {
-		xml.asInstanceOf[js.Dynamic].children.asInstanceOf[js.Array[dom.Node]].map(H2RNode(_, depth + 1))
+	val children: List[H2RNode] = {
+		xml.asInstanceOf[js.Dynamic].childNodes.asInstanceOf[js.Array[dom.Node]]
+			.map(H2RNode(_, depth + 1))
+			.filterNot(_.toString.trim.isEmpty)
+		    .toList
 	}
+
+	def textContent = xml.textContent.trim
 
 	val attributes: Seq[H2RAttribute] = {
 		UndefOr.any2undefOrA(xml.attributes).toOption match {
@@ -39,27 +47,48 @@ case class H2RNode(xml: dom.Node, depth: Integer) {
 	}
 
 	def childrenStr: String = {
-		if (children.nonEmpty) {
-			children.map(_.toString).mkString(",\n") match {
+		children match {
+			case Nil =>
+				""
+			case head :: Nil if head.xml.nodeName == "#text" =>
+				s"""($head)"""
 
-				case str if depth == -1 => str // root element
+			case _ =>
+				val cs = children.map(child => s"${"\t" * depth}$child").mkString(",\n")
 
-				case str =>
+				if (depth == 0) { // root node, don't add parenthesis, tabs nor new lines
+					cs
+				} else {
 					s"""(
-					    |${str}
-					    |$tabs)""".stripMargin
-			}
-		} else {
-			xml.textContent.trim match {
-				case textContent if textContent.nonEmpty => s"""("${textContent}")"""
-				case _ => ""
-			}
+					    |$cs
+				        |${"\t" * (depth-1)})""".stripMargin
+				}
 		}
+
 	}
 
-	def tabs = "\t" * depth
-
 	override def toString: String = {
-		s"""$tabs<.${xml.nodeName}${attributesStr}${childrenStr}""".stripMargin
+		xml.nodeName match {
+			case "#root" =>
+				childrenStr
+
+			case "#text" =>
+				if (textContent.nonEmpty) {
+					s""""$textContent""""
+				} else {
+					""
+				}
+
+			case "#comment" =>
+				if (textContent.nonEmpty) {
+					s"""//$textContent"""
+				} else {
+					""
+				}
+
+			case _ =>
+				s"""<.${xml.nodeName.toLowerCase}${attributesStr}${childrenStr}""".stripMargin
+		}
+
 	}
 }
